88872967dc6a71b7218aab36e1a330ac805ae21f
prashant2129 <prashantrakh38@gmail.com>
2025-07-31 18:31:03 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,17 @@
+list = []
+n = int(input("enter number of accountId:"))
+for i in range (0,n):
+    ele = int(input("enter accountID"))
+    list.append(ele)
+print(list)
+
+Ac_id=int(input("Enter Ac_id to searched"))
+flag=False
+for i in range (0,n):
+    if(list[i]==Ac_id):
+        print(Ac_id,"found at index",i)
+        flag = True
+        break
+4
+if flag==False:
+        print(Ac_id,"not Found")
